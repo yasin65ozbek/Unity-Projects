ab03a9a0c7600b4c99fd7916fdb3e3b6bfb2921e
Yasin ÖZBEK <39374741+yasin65ozbek@users.noreply.github.com>
2021-06-13 00:50:02 +0300

    Add files via upload

---
+++ ŞişeToplama/Scripts/kutuHareket.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class kutuHareket : MonoBehaviour
+{
+
+    public float hiz;
+    public Transform sise;//transform boyuttan konumdan vs. sorumludur
+    int puan;
+    public TextMeshProUGUI puanYazi;
+    public AudioSource siseAlma;
+
+    void Update()
+    {
+        puanYazi.text = "Puan : " + puan.ToString();
+
+        if (Input.GetKey(KeyCode.LeftArrow))//getKey->tuşa basılı tutuldukça
+        {
+            transform.Translate(-hiz * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(hiz * Time.deltaTime, 0f, 0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision temas)//Temas ettiği anda
+    {
+        float rastgele = Random.Range(-6f, 6f);
+
+        if (temas.gameObject.tag == "Sise")
+        {
+            sise.position = new Vector3(rastgele, 6f, 0f);
+            puan += 5;
+            siseAlma.Play();
+        }
+    }
+}
---
+++ ŞişeToplama/Scripts/sise.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class sise : MonoBehaviour
+{
+    public Transform sise2;
+    public int can;
+    public TextMeshProUGUI canYazi;
+    public GameObject bitisPaneli;
+    public AudioSource siseDusurme;
+
+    void Update()
+    {
+        canYazi.text = "Can : " + can.ToString();
+
+        if (can <= 0)
+        {
+            bitisPaneli.SetActive(true);
+            Time.timeScale = 0;
+            if (Input.anyKey)
+            {
+                Time.timeScale = 1;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision temas)
+    {
+        float rastgele = Random.Range(-6f, 6f);
+
+        if (temas.gameObject.tag == "Zemin")
+        {
+            sise2.position = new Vector3(rastgele, 6f, 0f);
+            can--;
+            siseDusurme.Play();
+        }
+    }
+}
